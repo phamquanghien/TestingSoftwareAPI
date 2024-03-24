e93a95d1123196f6d38950bfb4adbf9357fd6fec
phamquanghien <pqh.cntt@gmail.com>
2024-03-24 14:16:12 +0700

    CorsPolicy

--- Program.cs
+++ Program.cs
@@ -12,8 +12,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors( options => {
+    options.AddPolicy("CorsPolicy",
+    builder => builder
+        .AllowAnyMethod()
+        .AllowCredentials()
+        .SetIsOriginAllowed((host) => true)
+        .AllowAnyHeader());
+});
 
 var app = builder.Build();
+
+app.UseCors("CorsPolicy");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
